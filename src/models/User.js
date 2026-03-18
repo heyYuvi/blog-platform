f12f6a37398caae6854dfaa14984c4957c74d9ec
heyYuvi <prasadyuvraj39@gmail.com>
2026-03-18 20:00:37 +0530

    Added model and validation for User

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    avatar:{
+        type: String,
+        default: ""
+    }
+}, { timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+    
+export default User;
