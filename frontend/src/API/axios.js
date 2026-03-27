dcd179630f5d4a759fe0454fb2bee68909d68905
heyYuvi <prasadyuvraj39@gmail.com>
2026-03-27 13:30:12 +0530

    Added ProtectedRoute and Api interceptor

--- frontend/src/API/axios.js
+++ frontend/src/API/axios.js
@@ -4,4 +4,21 @@ const API = axios.create({
     baseURL: "http://localhost:5000/api/user"
 });
 
+API.interceptors.request.use((req) =>{
+    
+    try{
+        
+    const token = localStorage.getItem('token');
+
+    if(token){
+        req.headers.Authorization = `Bearer ${token}`
+    }
+    
+    return req;
+
+    }catch(error){
+        return Promise.reject(error);
+    }
+});
+
 export default API;
